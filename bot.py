3af6046d79bec87b7ea616fceab7a17b8a557c67
elaman <erkinbaevelaman35@gmail.com>
2025-03-19 12:29:28 +0300

    update version httpx and fix

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
 from config import BOT_TOKEN
 from handlers import admin, swaga, help, chat
+import httpx
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
@@ -13,6 +14,8 @@ dp.include_router(admin.router)
 dp.include_router(swaga.router)
 dp.include_router(help.router)
 dp.include_router(chat.router)
+
+print(f"HTTPX VERSION: {httpx.__version__}")
 
 async def main():
     print("🚀 Бот запускается...")
